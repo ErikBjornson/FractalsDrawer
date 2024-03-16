d7042ea9df550e068a0aadffd034c8c08e07e0ac
Andrey Chuvashov <ch.andrey.2345@gmail.com>
2024-03-16 16:53:58 +0300

    edit: refactoring Application.py

--- FractalsProject/src/Application.py
+++ FractalsProject/src/Application.py
@@ -3,20 +3,15 @@ import random
 
 class Application(tk.Tk):
 
-    def __init__(self):
+    def __init__(self) -> None:
 
         self.__mem = [] # array for storing coordinates of points
         self.__size = 450 # window size
-        self.__flag = True # point placement blocker
 
         super().__init__()
         self.title("Fractal")
         self.geometry(f"{self.__size}x{self.__size}")
         self.resizable(False, False)
-
-        main_menu = tk.Menu(self)
-        main_menu.add_command(label="Start", command=self.__fractalizator)
-        self.config(menu=main_menu)
 
         self.__canvas = tk.Canvas(
             background='black',
@@ -24,20 +19,19 @@ class Application(tk.Tk):
             height=self.__size - 4
         )
         self.__canvas.pack()
-        self.__canvas.bind("<Button-1>", self.__pointHolder)
+        self.__canvas.bind("<ButtonPress-1>", self.__pointHolder)
+        self.__canvas.bind("<Double-ButtonPress-1>", self.__fractalizator)
     
     # function of iterational process
-    def __fractalizator(self):
-        # checking the first and last press of the start button has been made
-        if len(self.__mem) != 4 or not self.__flag:
-            return
-        self.__flag = False
+    def __fractalizator(self, event) -> None:
+        # checking all points have been placed
+        if len(self.__mem) <= 3: return
         for _ in range(5000):
             self.__generator()
             self.update()
 
     # function of generating new points and constructing fractal
-    def __generator(self):
+    def __generator(self) -> None:
         r = random.randint(0, 2)
         new_point = self.__canvas.create_oval(
             (self.__mem[-1][0] + self.__mem[r][0]) // 2,
@@ -49,24 +43,19 @@ class Application(tk.Tk):
         self.__mem[-1] = self.__canvas.coords(new_point)
 
     # function of click-event
-    def __pointHolder(self, event):
-        # creating three reference points
-        if len(self.__mem) < 3:
-            self.__canvas.create_oval(
-                    event.x,
-                    event.y,
-                    event.x + 9,
-                    event.y + 9,
-                    outline='red', fill='white', width=2)
-        # creating a starting point
-        elif len(self.__mem) == 3:
-            self.__canvas.create_oval(
-                    event.x,
-                    event.y,
-                    event.x + 9,
-                    event.y + 9,
-                    outline='green', fill='white', width=2)
-        else:
+    def __pointHolder(self, event) -> None:
+        # checking not all points have been placeds
+        if(len(self.__mem)) > 3:
             return
+        # function for determining the color of points
+        Colorizer = lambda n: "red" if n < 3 else "green"
+        # creating points
+        self.__canvas.create_oval(
+            event.x,
+            event.y,
+            event.x + 9,
+            event.y + 9,
+            outline = Colorizer(len(self.__mem)),
+            fill='white', width=2)
         # memorizing coordinates of points
         self.__mem.append([event.x, event.y, event.x + 9, event.y + 9])
